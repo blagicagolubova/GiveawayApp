56e265214c32a33a3c77073a5be207ab5bcbb67a
AndjelaGolubova <golubova.andjela@gmail.com>
2021-02-19 23:19:52 +0100

    Login and Register

--- proekt/src/main/java/mk/ukim/finki/wp/proekt/ProektApplication.java
+++ proekt/src/main/java/mk/ukim/finki/wp/proekt/ProektApplication.java
@@ -2,12 +2,22 @@ package mk.ukim.finki.wp.proekt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@ServletComponentScan
 public class ProektApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProektApplication.class, args);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
     }
 
 }
